22c666970f289d8ceca2b46b6c1c97144940c58a
Bryan Hunt <bhunt@mac.com>
2014-04-05 16:43:47 -0500

    Cleaned out gh-pages for real this time

--- app/controllers/application.js
+++
@@ -1,8 +0,0 @@
-export default Ember.Controller.extend({
-  tabsMeta: [
-    Ember.Object.create({ title: "Vertical", linkTo: 'vertical'}),
-    Ember.Object.create({ title: "Horizontal", linkTo: 'horizontal'}),
-    Ember.Object.create({ title: "Vertical Composite", linkTo: 'verticalComposite'}),
-    Ember.Object.create({ title: "Composite", linkTo: 'composite'}),
-  ]
-});
